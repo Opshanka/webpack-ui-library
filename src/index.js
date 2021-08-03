0d8642dbffed1b0375ee035f13408f437ec1a103
Opshanka Prabath <opshanka.p@eight25media.com>
2021-08-03 08:29:22 +0530

    final product

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Dropdown from './webpack-ui/dropdown';
+import Tabs from './webpack-ui/tabs';
 import Tooltip from './webpack-ui/tooltip';
+import Snackbar from './webpack-ui/snackbar';
 
 //create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -11,4 +13,17 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
+});
+
+//create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
 });
